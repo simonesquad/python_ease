bb9b850937e278697b3d2635b93cd3ef2ef1dfd7
Simone Ballard <simone.a.ballard@gmail.com>
2022-04-23 15:58:05 -0500

    maze is rendering

--- path_finder/path_finder.py
+++ path_finder/path_finder.py
@@ -15,13 +15,21 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2, value, BLUE)
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    # stdscr.clear()
-    # stdscr.addstr(5, 5, "hello world!", blue_and_black)
-    # stdscr.refresh()
-    # stdscr.getch()
+    stdscr.clear()
+    print_maze(maze, stdscr)
+    stdscr.refresh()
+    stdscr.getch()
 
 wrapper(main)
