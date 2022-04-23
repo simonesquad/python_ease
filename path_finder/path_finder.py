60de85d70b746fffbaeef30ae33dd707d9109641
Simone Ballard <simone.a.ballard@gmail.com>
2022-04-23 15:23:20 -0500

    starting path finder grid

---
+++ path_finder/path_finder.py
@@ -0,0 +1,27 @@
+import curses
+from curses import wrapper
+import queue
+import time
+
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    # stdscr.clear()
+    # stdscr.addstr(5, 5, "hello world!", blue_and_black)
+    # stdscr.refresh()
+    # stdscr.getch()
+
+wrapper(main)
